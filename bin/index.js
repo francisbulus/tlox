6c4fca38451e4f4dc89388333f2a3688b3312f8f
“Francis <notchera@gmail.com>
2022-05-08 02:43:43 +0100

    update gitignore

--- bin/index.js
+++
@@ -1,2 +0,0 @@
-#!/usr/local/bin/node
-require('../build/src/lox');
--- src/error.ts
+++
@@ -1,5 +0,0 @@
-export const error = (line: number, msg: string): void => {
-  report(line, msg);
-};
-
-const report = (line: number, msg: string): void => {};
